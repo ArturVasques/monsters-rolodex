32d948a8533fd391b1884918eb316c0ba87d8e52
Artur Vasques <arturvasques@MacBook-Pro-de-Artur.local>
2020-05-29 16:56:34 +0100

    Updates

---
+++ precache-manifest.adf4a48becadcfaf7eb692990f4cfcde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4f3f0065ce60bab827c02ef74f20c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e8178032fdfc2c51e03",
+    "url": "/monsters-rolodex/static/css/main.88b5d4de.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8178032fdfc2c51e03",
+    "url": "/monsters-rolodex/static/js/main.4eadc47d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
